a4abd840e447a0d47d8e2e97da96107c9a0a38d3
Kelvin Fichter <kelvinfichter@gmail.com>
2018-12-23 15:52:30 -0500

    Started ChromeService

---
+++ src/services/chrome-service.js
@@ -0,0 +1,25 @@
+const BaseService = require('./base-service')
+
+class ChromeService extends BaseService {
+  constructor (options) {
+    super()
+  }
+
+  get name () {
+    return 'chrome-service'
+  }
+
+  handleRequest (request, sender, sendResponse) {
+    throw new Error('Not implemented')
+  }
+
+  start () {
+    chrome.runtime.onMessageExternal.addListener(this.handleRequest)
+  }
+
+  stop () {
+    chrome.runtime.onMessageExternal.removeListener(this.handleRequest)
+  }
+}
+
+module.exports = ChromeService
