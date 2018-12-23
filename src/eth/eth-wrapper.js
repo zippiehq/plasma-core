2ec0e7a97f2877b36cdd8630e2a273a9456aac91
Kelvin Fichter <kelvinfichter@gmail.com>
2018-12-22 19:49:26 -0500

    Started EthWrapper

--- src/eth/eth-wrapper.js
+++ src/eth/eth-wrapper.js
@@ -1,10 +1,43 @@
+const Web3 = require('web3')
+const abi = require('./abi.json')
+
+const CONTRACT_ADDRESS = '0x0' // TODO: Have this come from some config variable
+
 class EthWrapper {
+  constructor (provider) {
+    this.web3 = new Web3(provider)
+    const PlasmaContract = this.web3.eth.contract(abi)
+    this.contract = PlasmaContract.at(CONTRACT_ADDRESS)
+  }
+
   startExit () {
     throw Error('Not implemented')
   }
 
-  deposit () {
-    throw Error('Not implemented')
+  deposit (amount) {
+    return new Promise((resolve, reject) => {
+      this.contract.deposit({
+        value: amount
+      }, (err, res) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(res)
+        }
+      })
+    })
+  }
+
+  getBlock (number) {
+    return new Promise((resolve, reject) => {
+      this.contract.getBlock(number, (err, res) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(res)
+        }
+      })
+    })
   }
 }
 
