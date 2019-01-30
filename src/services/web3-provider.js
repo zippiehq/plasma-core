7e6bd34bb8ed39c67342b89a7ec0651931218381
Kelvin Fichter <kelvinfichter@gmail.com>
2019-01-29 19:38:56 -0500

    Add ability to change Ethereum endpoint

--- src/services/web3-provider.js
+++ src/services/web3-provider.js
@@ -1,11 +1,19 @@
 const Web3 = require('web3')
 const BaseService = require('./base-service')
 
+const defaultOptions = {
+  ethereumEndpoint: 'http://localhost:8545'
+}
+
 class Web3Provider extends BaseService {
+  constructor (options) {
+    super(options, defaultOptions)
+  }
+
   async start () {
     this.started = true
     this.web3 = new Web3(
-      new Web3.providers.HttpProvider('http://localhost:8545')
+      new Web3.providers.HttpProvider(this.options.ethereumEndpoint)
     )
     Object.assign(this, this.web3) // A bit of a hack, maybe there's a nicer way to expose this?
   }
